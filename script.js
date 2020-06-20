98055fbd4465aab3754b895f709bff49319cad98
megawall <prateekmahajan3@gmail.com>
2020-06-21 01:49:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+$('#education').click(()=>{
+    $('#content').load($('#education').attr('data-component'))
+})
+$('#skills').click(()=>{
+    $('#content').load($('#skills').attr('data-component'))
+})
+$('#experience').click(()=>{
+    $('#content').load($('#experience').attr('data-component'))
+})
+$('#about').click(()=>{
+    $('#content').load($('#about').attr('data-component'))
+})
+
+$('#edu').click(()=>{
+    $('#content').load($('#education').attr('data-component'))
+    $('#navbarSupportedContent').attr('class','collapse navbar-collapse')
+})
+$('#skls').click(()=>{
+    $('#content').load($('#skills').attr('data-component'))
+    $('#navbarSupportedContent').attr('class','collapse navbar-collapse')
+})
+$('#exp').click(()=>{
+    $('#content').load($('#experience').attr('data-component'))
+    $('#navbarSupportedContent').attr('class','collapse navbar-collapse')
+})
+$('#abt').click(()=>{
+    $('#content').load($('#about').attr('data-component'))
+    $('#btn').attr('aria-expanded',false)
+    $('#btn').attr('class','navbar-toggler collapsed')
+    $('#navbarSupportedContent').attr('class','collapse navbar-collapse')
+    
+})
